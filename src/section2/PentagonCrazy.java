1bc6bda4107f38d6156f465d9d3c1e58ba8a736a
League Student <league@iMac-11.attlocal.net>
2019-06-26 12:37:24 -0700

    four squar, zaydens_projoct,

--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -10,27 +10,33 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+Robot op= new Robot();
 		// 3. Put the robot's pen down
-
-		// 8. Make the robot go at maximum speed (100)
+op.penDown();
+op.setSpeed(99999);
+op.miniaturize();
+// 8. Make the robot go at maximum speed (100)
 
 		// 9. Set the pen to a color that you like for the shape
 
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+int num=5;
 		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-
+int ang=72;
 		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
+for (int i = 0; i < 200; i++) {
+	
 
 			// 2. Move the robot 200 pixels
-	
+	op.setRandomPenColor();
+	op.move(i);
 			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
 	
 			// 6. Turn the robot the amount in your angle variable
-	
+	op.turn(ang);
 			// 11. Turn the robot one more degree
-
+op.turn(1);
+}
 	}
 
 	// Variations:
